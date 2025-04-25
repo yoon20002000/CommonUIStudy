135d237018e52c18d1be69ba365179c33c2f93ec
yoon20002000 <yoon20002000@naver.com>
2025-04-25 23:05:11 +0900

    Build Config 설정
    임시 Widget 생성

--- Source/CommonUIStudy/CommonUIStudy.Build.cs
+++ Source/CommonUIStudy/CommonUIStudy.Build.cs
@@ -8,6 +8,12 @@ public class CommonUIStudy : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG",
+			"Slate",
+			"SlateCore",
+			"CommonUI"
+		});
 	}
 }
---
+++ Source/CommonUIStudy/UI/UI_StudyActivatableWidget.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonActivatableWidget.h"
+#include "UI_StudyActivatableWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMONUISTUDY_API UUI_StudyActivatableWidget : public UCommonActivatableWidget
+{
+	GENERATED_BODY()
+};
